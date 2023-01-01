c54ee2b61abf9cfc47fa7fda9fb9e78bfd92d511
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2023-01-01 02:37:35 -0400

    Update redditscrap.py
    Fixed indentation error and made input easier to enter.

--- redditscrap.py
+++ redditscrap.py
@@ -1,79 +1,69 @@
 import os
-import re 
+import re
 import requests
-import praw 
+import praw
 import configparser
 import concurrent.futures
 import argparse
 
+
 class redditImageScraper:
+    def __init__(self, sub, limit, order):
+        config = configparser.ConfigParser()
+        config.read('conf.ini')
+        self.sub = sub
+        self.limit = limit
+        self.order = order
+        self.nsfw = nsfw
+        self.path = f'images/{self.sub}/'
+        self.reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],
+                                  client_secret=config['REDDIT']['client_secret'],
+                                  user_agent='Anything You Want')
 
-	def __init__(self, sub, limit, order):
+    def download(self, image):
+        r = requests.get(image['url'])
+        with open(image['fname'], 'wb') as f:
+            f.write(r.content)
 
-		config = configparser.ConfigParser()
-		config,read('conf.ini')
-		self.sub = sub
-		self.limit = limit
-		self.order = order
-		self.path = f'images/{self.sub}/'
-		self.reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],
-			client_secret=config['REDDIT']['client_secret'],
-			user_agent='Anything You Want')
+    def start(self):
+        images = []
+        try:
+            go = 0
+            if self.order == 'hot':
+                submissions = self.reddit.subreddit(self.sub).hot(limit=None)
+            elif self.order == 'top':
+                submissions = self.reddit.subreddit(self.sub).top(limit=None)
+            elif self.order == 'new':
+                submissions = self.reddit.subreddit(self.sub).new(limit=None)
 
-		def download(self, image):
-			r = requests.get(image['url'])
+            for submission in submissions:
+                if not submission.stickied and submission.url.endswith(('jpg', 'jpeg', 'png' , 'gif')) :
+                    
+                    fname = self.path + re.search('(?s:.*)\w/(.*)', submission.url).group(1)
+                    if not os.path.isfile(fname):
+                        images.append({'url': submission.url, 'fname': fname})
+                        go += 1
+                        if go >= self.limit:
+                            break
+            if len(images):
+                if not os.path.exists(self.path):
+                    os.makedirs(self.path)
+                with concurrent.futures.ThreadPoolExecutor() as ptolemy:
+                    ptolemy.map(self.download, images)
 
-			with open(image['fname'], 'wb') as f:
-				f.write(r.content)
+            print("Scraping complete!")
+        except Exception as e:
+            print(e)
 
-		def start(self):
-
-			images = []
-
-			try:
-
-				go = 0
-
-				if self.order == 'hot':
-					submissions = self.reddit.subreddit(self.sub).hot(limit=None)
-				elif self.order =='top':
-					submissions== self.reddit.subreddit(self.sub).top(limit=None)
-				elif self.order =='new':
-					submissions = self.reddit.subreddit(self.sub).new(limit=None)
-
-				for submissions in submissions:
-
-					if not submissions.stickied and submissions.url.endswith(('jpg', 'jpeg', 'png', 'gif')):
-
-						fname = self.path + re.search('(?s:.*)\w/(.*)', submission.url).group(1)
-
-						if not os.path.isfile(fname):
-
-							images.append({'url': submission.url, 'fname': fname})
-							go += 1
-
-							if go >= self.limit:
-								break
-
-				if len(images):
-
-					if not os.path.exists(self.path):
-						os.makedirs(self.path)
-					with concurrent.futures.ThreadPoolExecutor() as ptolemy:
-						ptolemy.map(self.download, images)
-		except Exception as e:
-			print(e)
 
 def main():
 
-	parser = argparse.ArgumentParser(description='Reddit Image Downloader')
-	required_ags = parser.add_argument_group('required arguments')
-	required_ags.add_argument('-s', type=str, help='subreddit', required=True)
-	required_ags.add_argument('-n', type=int, help='number of images', required=True)
-	required_ags.add_argument('-o', type=str, help='order (new/top/hot)', required=True)
-	args.parser.parse_args()
-	scraper = redditImageScraper(args.s, args.n, args.o)
-	scraper.start()
+    print("Welcome to my Web Scraper!")
+    subreddit = input( "Which subreddit would you like to scrap?")
+    images = int(input("How many images would you like to get?"))
+    thread = input("Which thread would you like? (new/top/hot)")
+    scraper = redditImageScraper(subreddit, images, thread)
+    scraper.start()
 
 if __name__ == '__main__':
-	main()
+    main()
