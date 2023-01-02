f04404ef0862c8649e411a11223d41df99a73498
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2023-01-02 02:21:54 +0000

    Update redditscrap.py
    Added code to scrap comments data

--- redditscrap.py
+++ redditscrap.py
@@ -5,7 +5,91 @@ import praw
 import configparser
 import concurrent.futures
 import argparse
+import matplotlib.pyplot as plt
 
+class redditCommentScraper:
+    def __init__(self, sub, limit):
+        config = configparser.ConfigParser()
+        config.read('conf.ini')
+        self.limit = limit
+        self.sub = sub
+
+        # use the praw api and import the users key and secret keys 
+        self.reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],
+                                  client_secret=config['REDDIT']['client_secret'],
+                                  user_agent='Web Scrap')
+
+    def getComments(self):
+        subredditname = self.sub 
+
+        subreddit = self.reddit.subreddit(subredditname)
+        top_subbreddit = subreddit.top()
+        count = 0
+        max = 1000
+        print('auth success')
+        words = []
+        wordCount = {}
+
+        # list of common words- we ignore these in our count
+        commonWords = {'it','then', 'that','this','and','of','the','for','I','it','has','in',
+        'you','to','was','but','have','they','a','is','','be','on','are','an','or',
+        'at','as','do','if','your','not','can','my','their','them','they','with',
+        'at','about','would','like','there','You','from','get','just','more','so',
+        'me','more','out','up','some','will','how','one','what',"don't",'should',
+        'could','did','no','know','were','did',"it's",'This','he','The','we',
+        'all','when','had','see','his','him','who','by','her','she','our','thing','-',
+        'now','what','going','been','we',"I'm",'than','any','because','We','even',
+        'said','only','want','other','into','He','what','i','That','thought',
+        'think',"that's",'Is','much'}
+
+        for submission in subreddit.top(limit=500):
+            submission.comments.replace_more(limit=0)
+
+            for top_level_comment in submission.comments:
+                count+= 1
+
+                if(count == max):
+                    break
+                word = ""
+                for letter in top_level_comment.body:
+                    if(letter == ' '):
+                        if(word and not word[-1].isalnum()):
+                            word = word[:-1]
+                        if not word in commonWords:
+                            words.append(word)
+                        word = ""
+                    else:
+                        word += letter
+            if(count == max):
+                break
+
+        for word in words:
+            if word in wordCount:
+                wordCount[word] += 1
+            else:
+                wordCount[word] = 1
+
+        sortedList = sorted(wordCount, key = wordCount.get, reverse = True)
+
+        keyWords = []
+        keyCount = []
+        amount = 0 
+
+        for entry in sortedList:
+            keyWords.append(entry)
+            keyCount.append(wordCount[entry])
+            amount += 1
+
+            if(amount == 10):
+                break
+
+        labels = keyWords
+        sizes = keyCount
+
+        plt.title('Top Comments for: r/' + subredditname)
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        plt.axis('equal')
+        plt.show()
 
 class redditImageScraper:
     def __init__(self, sub, limit, order):
@@ -14,11 +98,12 @@ class redditImageScraper:
         self.sub = sub
         self.limit = limit
         self.order = order
-        self.nsfw = nsfw
         self.path = f'images/{self.sub}/'
+
+        # use the praw api and import the users key and secret keys 
         self.reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],
                                   client_secret=config['REDDIT']['client_secret'],
-                                  user_agent='Anything You Want')
+                                  user_agent='Web Scrap')
 
     def download(self, image):
         r = requests.get(image['url'])
@@ -26,9 +111,13 @@ class redditImageScraper:
             f.write(r.content)
 
     def start(self):
+
+        print("Starting to scrap. This make take a few seconds depending on how many images you want.")
         images = []
         try:
             go = 0
+
+            # how will we sort the posts
             if self.order == 'hot':
                 submissions = self.reddit.subreddit(self.sub).hot(limit=None)
             elif self.order == 'top':
@@ -43,10 +132,14 @@ class redditImageScraper:
                     if not os.path.isfile(fname):
                         images.append({'url': submission.url, 'fname': fname})
                         go += 1
+
+                        # once we have collected enough images quit the loop
                         if go >= self.limit:
                             break
             if len(images):
+
                 if not os.path.exists(self.path):
+                    # make a new directory to store images in
                     os.makedirs(self.path)
                 with concurrent.futures.ThreadPoolExecutor() as ptolemy:
                     ptolemy.map(self.download, images)
@@ -55,7 +148,6 @@ class redditImageScraper:
         except Exception as e:
             print(e)
 
-
 def main():
 
     print("Welcome to my Web Scraper!")
@@ -63,6 +155,9 @@ def main():
     images = int(input("How many images would you like to get?"))
     thread = input("Which thread would you like? (new/top/hot)")
     scraper = redditImageScraper(subreddit, images, thread)
+
+    commentscraper = redditCommentScraper(subreddit, images)
+    # commentscraper.getComments()
     scraper.start()
 
 if __name__ == '__main__':
